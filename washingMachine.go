21704f0de2a1294f06a9933b7d06704ce04ca2eb
ProhvetIV <70652961+ProhvetIV@users.noreply.github.com>
2023-07-14 15:23:38 +0300

    Create washingMachine.go

---
+++ washingMachine.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// washingmachine struct
+type washingmachine struct {
+	brand string
+	model string
+}
+
+// func to get the brand and model, returning a string
+func (wm washingmachine) getModel() string {
+	return fmt.Sprintf("Your washingmachine is from %s and the model is %s", wm.brand, wm.model)
+}
+
+// func to print the getModel() sting in the terminal
+func test(info washingmachine) {
+	fmt.Println(info.getModel())
+	fmt.Println("========================================================================")
+}
+
+func main() {
+	test(washingmachine{
+		brand: "Samsung",
+		model: "Eco-Life 205",
+	})
+	test(washingmachine{
+		brand: "Sony",
+		model: "The-Fastest 08",
+	})
+	test(washingmachine{
+		brand: "Honda",
+		model: "Multi-Purpose C960A",
+	})
+}
